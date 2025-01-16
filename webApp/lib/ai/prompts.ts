640ffd1683046e3b28fe61d84a98d2cbb77d1259
Sudhanshu Pandey <sp6370@nyu.edu>
2025-01-16 16:14:18 +0000

    chore: add debug log for prompt

--- webApp/lib/ai/prompts.ts
+++ webApp/lib/ai/prompts.ts
@@ -1,3 +1,5 @@
 export const regularPrompt = process.env.REGULAR_PROMPT || `You are an helpful assistant.`;
 
+console.log(`regularPrompt: ${regularPrompt}`);
+
 export const systemPrompt = `${regularPrompt}`;
