ee97a052e95576dd9d7c6a2acec5070b6b58a731
Sudhanshu Pandey <sp6370@nyu.edu>
2025-01-16 15:53:05 +0000

    chore: update the prmopt config

--- webApp/lib/ai/prompts.ts
+++ webApp/lib/ai/prompts.ts
@@ -1,4 +1,3 @@
-export const regularPrompt =
-  'You are a friendly assistant! Keep your responses concise and helpful.';
+export const regularPrompt = process.env.REGULAR_PROMPT || `You are an helpful assistant.`;
 
 export const systemPrompt = `${regularPrompt}`;
