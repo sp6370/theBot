cd672260c78d157412f6abd46eebe28ca841f7c2
Sudhanshu Pandey <sp6370@nyu.edu>
2025-01-16 00:19:00 +0000

    chore: update the model config

--- webApp/lib/ai/models.ts
+++ webApp/lib/ai/models.ts
@@ -9,11 +9,11 @@ export interface Model {
 
 export const models: Array<Model> = [
   {
-    id: 'llama3.2-1B-ollama',
-    label: 'Llama 3.2',
-    apiIdentifier: 'llama3.2:1b',
-    description: 'Small 1B model for fast, lightweight tasks',
+    id: 'llama3.1-8B',
+    label: 'Llama 3.1',
+    apiIdentifier: 'Meta-Llama-3.1-8B-Instruct-quantized.w4a16',
+    description: 'Small 8B model for fast, lightweight tasks',
   },
 ] as const;
 
-export const DEFAULT_MODEL_NAME: string = 'llama3.2-1B-ollama';
+export const DEFAULT_MODEL_NAME: string = 'llama3.1-8B';
